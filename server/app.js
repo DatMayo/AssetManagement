2e78a6ef031164df1eb1bfac770af749261ee5d3
Mario Franze <mfranze@germandev.de>
2018-12-26 11:52:09 +0100

    + Added body-parser + Added Session Handling + Added routes + Added helmet - Removed unused addons/modules

--- server/app.js
+++ server/app.js
@@ -1,9 +1,9 @@
 require('dotenv').load();
 
+const bodyParser = require('body-parser');
 const express = require('express');
 const helmet = require('helmet');
 const Logger = require('./utils/Logger');
-const path = require('path');
 const session = require('express-session');
 const sequelize = require('sequelize');
 
@@ -17,6 +17,7 @@ const connection = new sequelize(process.env.DBDB, process.env.DBUSER, process.e
 	});
 const port = process.env.port || 3000;
 
+// #region Table-Structure
 const Accounts = connection.define('Accounts',
 	{
 		ID:
@@ -73,25 +74,33 @@ const State = connection.define('State',
 		CategoryID: sequelize.INTEGER,
 		NextState: sequelize.INTEGER,
 	});
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+// #endregion
+
+let SesData = null;
+SesData = { };
 
 app.set('AccountSQL', Accounts);
 app.set('AssetSQL', Asset);
 app.set('CategorySQL', Category);
 app.set('SecuritySQL', Security);
 app.set('StateSQL', State);
-
-app.use(require('./routes/admin/getAccounts'));
+app.set('SessionData', SesData);
 
 app.use(helmet());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
 app.use(session({
 	secret: require('crypto').randomBytes(64).toString('hex'),
 	resave: false,
 	saveUninitialized: true,
 	expires: new Date(Date.now() + (30 * 86400 * 1000)),
 }));
+
+app.use(require('./routes/admin/getAccounts'));
+
+app.use(require('./routes/category/create'));
+app.use(require('./routes/category/getAll'));
 
 connection.sync().then(() =>
 {
